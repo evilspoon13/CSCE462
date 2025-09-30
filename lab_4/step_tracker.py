d9e5e57a5f55dc2ef1c383aa2e992bb88643a910
cameron28202 <cameron28202@gmail.com>
2025-09-30 20:00:59 +0100

    done lab 4

--- lab_4/step_tracker.py
+++ lab_4/step_tracker.py
@@ -5,11 +5,33 @@ import adafruit_mpu6050
 from time import sleep, perf_counter
 i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
-# Write a loop to poll each sensor and print its axis values
-while True:
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" %
-    (mpu.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (mpu.gyro))
-    print("")
 
+
+ACCEL_THRESHOLD_HIGH = 2.0
+ACCEL_THRESHOLD_LOW = -2.0
+DEBOUNCE_TIME = 0.2 # min time between steps
+
+
+num_steps = 0
+last_step_time = 0
+step_state = "WAITING_FOR_PEAK"
+
+while True:
+    current_time = perf_counter()
+    acceleration = mpu.acceleration[2] - 9.8
+    
+    time_since_last_step = current_time - last_step_time
+    
+    if step_state == "WAITING_FOR_PEAK":
+        if acceleration > ACCEL_THRESHOLD_HIGH and time_since_last_step > DEBOUNCE_TIME:
+            step_state = "WAITING_FOR_MIN"
+            
+    elif step_state == "WAITING_FOR_MIN":
+        if acceleration < ACCEL_THRESHOLD_LOW:
+            num_steps += 1
+            last_step_time = current_time
+            step_state = "WAITING_FOR_PEAK"
+    
+    print("Steps: ", num_steps)
+    
     sleep(0.1)
